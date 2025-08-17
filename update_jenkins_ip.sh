7338833e475dee1fb48f2a2b64659e7544e5a2d7
vinayak4393 <vinaykwod4393@gmail.com>
2025-08-17 23:26:08 +0530

    update 17-August-25 11:26PM

--- update_jenkins_ip.sh
+++ update_jenkins_ip.sh
@@ -4,3 +4,9 @@ echo "<?xml version='1.1' encoding='UTF-8'?>
 <jenkins.model.JenkinsLocationConfiguration>
         <jenkinsUrl>http://(curl -s http://checkip.amazonaws.com):8080/</jenkinsUrl>
         </jenkins.model.JenkinsLocationConfiguration>" > /var/lib/jenkins/jenkins.model.JenkinsLocationConfiguration.xml
+
+
+# chmod +x update_jenkins_ip.sh
+
+# crontab -e 
+# @reboot sudo bash /home/ubuntu/update_jenkins_ip.sh
